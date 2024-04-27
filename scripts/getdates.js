a676d7ba9abdc815191a6ec5dd9e1a8d5cc97e7f
hayctama <hay23019@byui.edu>
2024-04-26 18:46:06 -0600

    checking homepage changes

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+
+
+const currentYear = document.getElementById('currentyear');
+const lastModified = document.getElementById('lastModified');
+
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = `Last modification: ${document.lastModified}`;
