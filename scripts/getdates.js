b283cadc7ed031640d7313a9fddc8eb56844b20b
hayctama <hay23019@byui.edu>
2024-04-26 20:34:29 -0600

    updates to homepage

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,15 @@
+// Year
+let year = new Date();
+currentYear.innerHTML = year.getFullYear();
+
+// Last modified
+updated.innerHTML = document.lastModified;
+
+// const currentYear = document.getElementById('currentyear');
+// currentYear.textContent = new Date().getFullYear();
 
 
-const currentYear = document.getElementById('currentyear');
-const lastModified = document.getElementById('lastModified');
+// const lastModified = document.getElementById('lastModified');
+// lastModified.textContent = `Last modification: ${document.lastModified}`;
 
-currentYear.textContent = new Date().getFullYear();
-lastModified.textContent = `Last modification: ${document.lastModified}`;
+let currentYear = document.querySelector(".currentYear");
